a874c157895f5f2aefc1aec909ccea58f7f05b16
Henry Anorue <hanorue@drohealth.com>
2025-11-16 14:32:27 +0100

    Class creation

---
+++ Messages.cs
@@ -0,0 +1,21 @@
+
+namespace MyFirstProj
+{
+    class Messages
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello! Welcome to Csharp programming Language");
+        }
+        public static void Waitng()
+        {
+            Console.WriteLine("I am waiting for my meeting by 4pm");
+        }
+        public static void Bye()
+        {
+            Console.WriteLine("Thanks for banking with us, bye!");
+        }
+
+    }
+   
+}
--- MyFirstProj.cs
+++ MyFirstProj.cs
@@ -5,13 +5,12 @@ namespace MyFirstProj
     {
         static void Main (string[] args)
         {
-        // double marketPrice  = Checkout(3.23, 4.54, 2.34, 8.9, 4.67);
-        // Console.WriteLine("Market Price is: " + marketPrice);
-        double temperature = 20;
-        string message;
-        message = temperature >= 15 ? "Its warm outside" : "Its cold outside";
-        Console.WriteLine(message);
+            // class = A bundle of related code. Can be used as blueprint to create objects (OOP)
         
+        Messages.Hello();
+        Messages.Waitng();
+        Messages.Bye();
+
         }
 
     }
